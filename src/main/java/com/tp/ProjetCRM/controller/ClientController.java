30d7123ab1cd65934a668960290a8534d83d8bfb
AHamon47 <ahamon47@gmail.com>
2022-11-29 14:27:50 +0100

    ajout des controllers order et client

---
+++ src/main/java/com/tp/ProjetCRM/controller/ClientController.java
@@ -0,0 +1,71 @@
+package com.tp.ProjetCRM.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tp.ProjetCRM.model.Client;
+import com.tp.ProjetCRM.service.ClientDirectory;
+
+@RestController
+@RequestMapping("api")
+public class ClientController {
+
+	@Autowired
+	private ClientDirectory clientDirectory;
+
+	@GetMapping("clients")
+	public List<Client> getClients() {
+		return clientDirectory.getClients();
+	}
+
+	@GetMapping("clients/{id}")
+	public ResponseEntity<Client> getClient(@PathVariable("id") Long id) {
+		Optional<Client> optionalClient = clientDirectory.getClient(id);
+		if (optionalClient.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		} else {
+			return ResponseEntity.ok(optionalClient.get());
+		}
+	}
+
+	@PostMapping("clients")
+	public Client postClient(@RequestBody Client newClient) {
+		clientDirectory.addClient(newClient);
+		return newClient;
+	}
+
+	@DeleteMapping("clients/{id}")
+	public void deleteClient(@PathVariable("id") Long id) {
+		clientDirectory.deleteClient(id);
+	}
+
+	@PutMapping("clients/{id}")
+	public ResponseEntity updateClient(@RequestBody Client client, @PathVariable("id") Long id) {
+
+		if (id != client.getId()) {
+			return ResponseEntity.badRequest().build();
+		} else {
+
+			clientDirectory.updateClient(client, id);
+			return ResponseEntity.ok().build();
+		}
+	}
+
+	@PatchMapping("clients/{id}")
+	public void patchClient(@RequestBody Client client, @PathVariable("id") Long id) {
+		System.out.println(client);
+		clientDirectory.patchClient(client, id);
+	}
+}
---
+++ src/main/java/com/tp/ProjetCRM/controller/OrderController.java
@@ -0,0 +1,70 @@
+package com.tp.ProjetCRM.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tp.ProjetCRM.model.Order;
+import com.tp.ProjetCRM.service.OrderDirectory;
+
+@RestController
+@RequestMapping("api")
+public class OrderController {
+	@Autowired
+	private OrderDirectory orderDirectory;
+
+	@GetMapping("orders")
+	public List<Order> getOrders() {
+		return orderDirectory.getOrders();
+	}
+
+	@GetMapping("orders/{id}")
+	public ResponseEntity<Order> getOrder(@PathVariable("id") Long id) {
+		Optional<Order> optionalOrder = orderDirectory.getOrder(id);
+		if (optionalOrder.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		} else {
+			return ResponseEntity.ok(optionalOrder.get());
+		}
+	}
+
+	@PostMapping("orders")
+	public Order postOrder(@RequestBody Order newOrder) {
+		orderDirectory.addOrder(newOrder);
+		return newOrder;
+	}
+
+	@DeleteMapping("orders/{id}")
+	public void deleteOrder(@PathVariable("id") Long id) {
+		orderDirectory.deleteOrder(id);
+	}
+
+	@PutMapping("orders/{id}")
+	public ResponseEntity updateOrder(@RequestBody Order order, @PathVariable("id") Long id) {
+
+		if (id != order.getId()) {
+			return ResponseEntity.badRequest().build();
+		} else {
+
+			orderDirectory.updateOrder(order, id);
+			return ResponseEntity.ok().build();
+		}
+	}
+
+	@PatchMapping("orders/{id}")
+	public void patchOrder(@RequestBody Order order, @PathVariable("id") Long id) {
+		System.out.println(order);
+		orderDirectory.patchOrder(order, id);
+	}
+}
