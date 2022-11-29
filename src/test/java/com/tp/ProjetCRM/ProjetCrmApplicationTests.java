ad7f63eaced785315495ba0eed99a56f4ab1d1fc
AHamon47 <ahamon47@gmail.com>
2022-11-29 11:48:04 +0100

    first commit

---
+++ src/test/java/com/tp/ProjetCRM/ProjetCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.ProjetCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
